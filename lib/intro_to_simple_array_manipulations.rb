4e9da624fca881d76d1a1c1072aa70a3d5efa71b
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-12 18:50:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,4 +7,14 @@ end
 def using_pop(array)
   array.pop
 end
-def 
+def pop_with_args(array)
+  array.pop(2)
+end
+def using_shift(array)
+  array.shift
+end
+def shift_with_args(array)
+  array.shift(2)
+end
+
+  
